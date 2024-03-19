aac441fd1218e5f6a9a60ff5b80f05ca5071a402
Marco Kellershoff <marco@kellershoff.net>
2024-03-19 18:03:37 +0100

    📦 Update languageservers

--- nvim/lua/plugins/lsp_config/init.lua
+++ nvim/lua/plugins/lsp_config/init.lua
@@ -5,7 +5,23 @@ vim.api.nvim_command('inoremap <C-n> <C-x><C-o>')
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local servers = { 'tsserver', 'html', 'eslint', 'cssls', 'jsonls', 'gopls', 'terraformls', 'bashls', 'dockerls', 'yamlls', 'vimls', 'sqlls' , 'rust_analyzer'}
+local servers = {
+  'bashls',
+  'cssls',
+  'dockerls',
+  'eslint',
+  'gopls',
+  'html',
+  'jsonls',
+  'rust_analyzer',
+  'sqlls',
+  'svelte',
+  'tailwindcss',
+  'terraformls',
+  'tsserver',
+  'vimls',
+  'yamlls',
+}
 
 for _, lsp in ipairs(servers) do
 	if nvim_lsp[lsp] ~= nil then
