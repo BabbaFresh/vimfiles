043c0b81df8f486273bc69813a6c7c85ddfd0edc
Marco Kellershoff <marco@kellershoff.net>
2024-06-13 18:19:27 +0200

    chore: update plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -186,9 +186,11 @@ require('lazy').setup({
     'mistweaverco/bluloco.nvim',
     lazy = false,
     priority = 1000,
+    tag = 'v1.0.0',
     config = function()
       require('bluloco').setup({
         theme = 'dark',
+        italic_comments = true,
       })
     end,
   },
