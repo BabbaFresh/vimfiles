16d14041facc39afd1ecde190e77a4f961188c28
Marco Kellershoff <marco@kellershoff.net>
2024-06-25 00:32:43 +0200

    feat: add ft override

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
 require 'plugins'
 require 'config'
+require 'config.filetypes'
 require 'config.keymap'
---
+++ nvim/lua/config/filetypes.lua
@@ -0,0 +1,9 @@
+vim.filetype.add({
+  extension = {
+    ['http'] = 'http',
+  },
+  filename = {
+    ['tsconfig.json'] = 'jsonc',
+    ['.env'] = 'bash',
+  },
+})
