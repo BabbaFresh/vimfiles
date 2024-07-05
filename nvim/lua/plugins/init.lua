7b9367f729b88cd91aa07ff18f7e8f71b647f729
Marco Kellershoff <marco@kellershoff.net>
2024-07-05 17:35:54 +0200

    add noice.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -45,6 +45,30 @@ require('lazy').setup({
     }
   },
 
+  -- Nicer messages and commandline
+  {
+    "folke/noice.nvim",
+    config = function()
+      event = "VeryLazy",
+      require("noice").setup({
+        lsp = {
+          signature = { enabled = false },
+        },
+        presets = {
+          bottom_search = false, -- use a classic bottom cmdline for search
+          command_palette = false, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  },
+
   -- Navigate code on steroids with flash.nvim ⚡
   {
     'folke/flash.nvim',
