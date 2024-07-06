026fe6f615e05ac39cc0e843b50814456e2edfa1
Marco Kellershoff <marco@kellershoff.net>
2024-07-06 11:55:04 +0200

    fix(kulala): missing setup call

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -87,6 +87,9 @@ require('lazy').setup({
   -- Simple HTTP Client
   {
     'mistweaverco/kulala.nvim',
+    config = function()
+      require('kulala').setup()
+    end,
     dir = "/home/marco/projects/personal/kulala.nvim",
   },
 
