afab9a1b65a3c980dee216875c9c14cc5c8045b0
Marco Kellershoff <marco@kellershoff.net>
2024-09-09 20:12:19 +0200

    update keymap for http

--- nvim/ftplugin/http.lua
+++ nvim/ftplugin/http.lua
@@ -1,3 +1,55 @@
-vim.api.nvim_set_keymap("n", "<C-j>", ":lua require('kulala').jump_next()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", ":lua require('kulala').jump_prev()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", ":lua require('kulala').run()<CR>", { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "<C-l>",
+  "<cmd>lua require('kulala').run()<cr>",
+  { noremap = true, silent = true, desc = "Send the request" }
+)
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "[",
+  "<cmd>lua require('kulala').jump_prev()<cr>",
+  { noremap = true, silent = true, desc = "Jump to the previous request" }
+)
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "]",
+  "<cmd>lua require('kulala').jump_next()<cr>",
+  { noremap = true, silent = true, desc = "Jump to the next request" }
+)
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "<leader>i",
+  "<cmd>lua require('kulala').inspect()<cr>",
+  { noremap = true, silent = true, desc = "Inspect the current request" }
+)
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "<leader>t",
+  "<cmd>lua require('kulala').toggle_view()<cr>",
+  { noremap = true, silent = true, desc = "Toggle between headers and body view" }
+)
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "<leader>co",
+  "<cmd>lua require('kulala').copy()<cr>",
+  { noremap = true, silent = true, desc = "Copy the current request as curl" }
+)
+
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  "<leader>ci",
+  "<cmd>lua require('kulala').from_curl()<cr>",
+  { noremap = true, silent = true, desc = "Pastes curl from clipboard as http request" }
+)
