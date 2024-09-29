d03f03e05c76962b600f88b840e7bb75769aec6a
Marco Kellershoff <marco@kellershoff.net>
2024-09-29 15:22:45 +0200

    chore(themes): add retro theme

--- nvim/lua/plugins/config/retro-theme-nvim.lua
+++ nvim/lua/plugins/config/retro-theme-nvim.lua
@@ -1,6 +1,10 @@
- return {
-   'mistweaverco/retro-theme.nvim',
-   lazy = false,
-   priority = 1000,
-   dir = '/home/marco/projects/personal/retro-theme.nvim',
- }
+return {
+  "mistweaverco/retro-theme.nvim",
+  lazy = false,
+  priority = 1000,
+  dir = "/home/marco/projects/personal/retro-theme.nvim",
+  opts = {
+    hot_reload = false,
+    cache = true,
+  },
+}
