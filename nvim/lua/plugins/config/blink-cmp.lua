09bd5eccb09ce030ebbb0d8bdf3997fa39993625
Marco Kellershoff <marco@kellershoff.net>
2025-01-31 12:56:25 +0100

    feat(pkg): update

--- nvim/lua/plugins/config/blink-cmp.lua
+++ nvim/lua/plugins/config/blink-cmp.lua
@@ -2,13 +2,7 @@ return {
   "saghen/blink.cmp",
 
   build = "cargo build --release",
-
-  -- use a release tag to download pre-built binaries
   version = "*",
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -34,8 +28,11 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { "lsp", "path", "buffer" },
+      default = {
+        "lsp",
+        "path",
+        "buffer",
+      },
     },
   },
-  opts_extend = { "sources.default" },
 }
