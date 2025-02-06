ec350a8d8b2f41364b2132804bbfd6f0a6830ffa
Marco Kellershoff <marco@kellershoff.net>
2025-02-06 07:03:57 +0100

    feat(keymap): make quitting and saving easier

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -7,8 +7,14 @@ map("", "k", "gk")
 -- Save
 map("n", "<leader>w", ":w<CR>")
 
+-- Save and quit buffer
+map("n", "<leader>wq", ":wq<CR>")
+
+-- Save and quit Neovim
+map("n", "<leader>q", ":wqall!<CR>")
+
 -- Quit out of buffer
-map("n", "<leader>q", ":bd!<CR>")
+map("n", "<leader>x", ":bd!<CR>")
 
 -- Replacement for ctrlp
 map("n", "<C-p>", ":Telescope find_files<CR>")
