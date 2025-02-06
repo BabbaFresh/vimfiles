48ef6671766f7c51d383c29b0cb14cb38ca2439c
Marco Kellershoff <marco@kellershoff.net>
2025-02-06 07:24:58 +0100

    feat(plugins): add noice again

---
+++ nvim/lua/plugins/config/noice-nvim.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = {
+      -- override markdown rendering so plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+      },
+    },
+    presets = {
+      command_palette = false, -- position the cmdline and popupmenu together
+    },
+  },
+}
